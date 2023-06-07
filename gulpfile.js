e6b145ea5cb2f265bd694148663e477544151996
belabela <belabela@Xubuntu.myguest.virtualbox.org>
2023-06-06 21:05:34 -0300

    Initial setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('node-sass'));
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('sass', function () {
-  return gulp.src('src/scss/*.scss')
+gulp.task('sass', function() {
+  return gulp.src('src/scss/custom.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('default', function () {
-  gulp.watch('src/scss/*.scss', gulp.series('sass'));
+gulp.task('watch', function() {
+  gulp.watch('src/scss/*/.scss', gulp.series('sass'));
 });
+
+gulp.task('default', gulp.series('sass', 'watch'));
